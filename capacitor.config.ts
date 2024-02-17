bd60e44c0d70f2ad1cd34f9f6da25853fc03f6a4
Rashid Ali <rashid.ali@carbonplace.com>
2024-02-18 00:23:44 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'voltaapp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
