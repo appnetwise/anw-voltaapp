86f9b4fb8a581785e31efb3c4e4c2c6a8adc385a
Rashid Ali <rashid.ali@carbonplace.com>
2024-02-18 14:57:50 +0530

    fix: rxjs issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FirebaseX } from '@awesome-cordova-plugins/firebase-x';
+import { FirebaseX } from '@ionic-native/firebase-x/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    FirebaseX as any,
+    FirebaseX,
   ],
   bootstrap: [AppComponent],
 })
