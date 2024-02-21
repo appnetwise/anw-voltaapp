8fcd9651cc5d31b137c7dbc4896e02fb1463c1cf
Rashid Ali <rashid.ali@carbonplace.com>
2024-02-21 22:08:50 +0530

    Push notification - completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,72 @@
 import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { FirebaseX } from '@ionic-native/firebase-x/ngx';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
+declare var cordova: any;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private platform: Platform, private firebase: FirebaseX) {
+  constructor(
+    private platform: Platform,
+    private firebase: FirebaseX,
+    private localNotifications: LocalNotifications
+  ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      // Initialize Firebase plugin here
-      this.firebase.onMessageReceived().subscribe((data) => {
-        // Handle incoming push notifications
-        console.log(data);
+    document.addEventListener('deviceready', () => {
+      this.platform.ready().then(() => {
+        // Ask for notification permissions
+        this.askForNotificationPermissions();
+
+        // Get and log the current FCM token
+
+        this.firebase.onMessageReceived().subscribe((data) => {
+          // Handle incoming push notifications
+          console.log('Schedule Push Notification');
+          this.localNotifications.schedule({
+            text: data.body,
+            title: data.title,
+            foreground: true,
+
+            // Add more properties as needed
+          });
+          // cordova.plugins.notification.local.schedule({
+          //   title: data.title,
+          //   text: data.text,
+          // });
+        });
+
+        // Listen for token refresh events
+        this.onTokenRefresh();
       });
     });
+  }
+  askForNotificationPermissions() {
+    if (this.platform.is('cordova')) {
+      this.firebase
+        .grantPermission()
+        .then(() => {
+          console.log('Push notifications permission granted');
+          // You can handle additional logic here if needed
+        })
+        .catch((error) => {
+          console.error('Error granting push notifications permission', error);
+          // Handle the case where the user denied permission
+        });
+    }
+  }
+
+  onTokenRefresh() {
+    if (this.platform.is('cordova')) {
+      this.firebase.onTokenRefresh().subscribe((token) => {
+        console.log('Refreshed FCM Token:', token);
+      });
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FirebaseX } from '@ionic-native/firebase-x/ngx';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +15,7 @@ import { FirebaseX } from '@ionic-native/firebase-x/ngx';
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     FirebaseX,
+    LocalNotifications,
   ],
   bootstrap: [AppComponent],
 })
