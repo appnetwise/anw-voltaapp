0f9e6f3dd0ebb4f242a2e107013d1a145d13492c
Rashid Ali <rashid.ali@carbonplace.com>
2024-02-22 18:29:32 +0530

    In app notification changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { FirebaseX } from '@ionic-native/firebase-x/ngx';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { HttpClient } from '@angular/common/http';
 
-declare var cordova: any;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  badgeValue: number = 1; // Initialize badge value
+  showNotificationDetails: boolean = false;
+  selectedNotification: any;
+
   constructor(
     private platform: Platform,
     private firebase: FirebaseX,
-    private localNotifications: LocalNotifications
+    private localNotifications: LocalNotifications,
+    private http: HttpClient,
+    private ngZone: NgZone
   ) {
     this.initializeApp();
   }
@@ -26,20 +32,28 @@ export class AppComponent {
 
         // Get and log the current FCM token
 
-        this.firebase.onMessageReceived().subscribe((data) => {
-          // Handle incoming push notifications
-          console.log('Schedule Push Notification');
-          this.localNotifications.schedule({
-            text: data.body,
-            title: data.title,
-            foreground: true,
+        // Subscribe all users
+        this.subscribeAll();
 
-            // Add more properties as needed
+        this.firebase.onMessageReceived().subscribe((data) => {
+          this.ngZone.run(() => {
+            // Handle incoming push notifications
+            console.log('Schedule Push Notification');
+            this.localNotifications.schedule({
+              text: data.body,
+              title: data.title,
+              foreground: true,
+            });
+
+            // Update badge value
+            this.badgeValue++;
+
+            // Store notification details
+            this.selectedNotification = {
+              title: data?.title,
+              text: data?.body,
+            };
           });
-          // cordova.plugins.notification.local.schedule({
-          //   title: data.title,
-          //   text: data.text,
-          // });
         });
 
         // Listen for token refresh events
@@ -47,17 +61,16 @@ export class AppComponent {
       });
     });
   }
+
   askForNotificationPermissions() {
     if (this.platform.is('cordova')) {
       this.firebase
         .grantPermission()
         .then(() => {
           console.log('Push notifications permission granted');
-          // You can handle additional logic here if needed
         })
         .catch((error) => {
           console.error('Error granting push notifications permission', error);
-          // Handle the case where the user denied permission
         });
     }
   }
@@ -68,5 +81,54 @@ export class AppComponent {
         console.log('Refreshed FCM Token:', token);
       });
     }
+  }
+
+  postTokenToApi(token: string) {
+    if (token) {
+      const apiEndpoint =
+        'https://4911-2405-201-c04a-70c3-907b-e8c9-400-162b.ngrok-free.app/subscribe-all';
+
+      const formData = new FormData();
+      formData.append('token', token);
+
+      this.http.post(apiEndpoint, formData).subscribe(
+        (response) => {
+          console.log('Subscribe all token posted successfully:', response);
+        },
+        (error: Error) => {
+          console.error(
+            'Error posting subscribe all token to API:',
+            error.message
+          );
+        }
+      );
+    } else {
+      console.error('FCM Token is null');
+    }
+  }
+
+  subscribeAll() {
+    if (this.platform.is('cordova')) {
+      this.firebase
+        .getToken()
+        .then((token) => {
+          if (token) {
+            console.log('Sending notification for FCM Token:', token);
+            this.postTokenToApi(token);
+          } else {
+            console.error('FCM Token is null');
+          }
+        })
+        .catch((error) => {
+          console.error('Error getting FCM token', error);
+        });
+    }
+  }
+
+  showNotifications() {
+    // Toggle the display of notification details
+    this.ngZone.run(() => {
+      this.showNotificationDetails = !this.showNotificationDetails;
+    });
   }
 }
