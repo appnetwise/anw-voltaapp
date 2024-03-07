251cbcbd5a3d51849d03d828e7500ed4d2615fe3
Rashid Ali <rashid.ali@carbonplace.com>
2024-03-07 15:12:58 +0530

    updated api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://4911-2405-201-c04a-70c3-907b-e8c9-400-162b.ngrok-free.app',
+  apiUrl: 'https://d1b5-2405-201-c04a-70c3-a840-ab3d-e60-b7.ngrok-free.app/',
 };
